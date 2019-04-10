d8a264b0b8327cef5d96b6633659ba82ca8831a2
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-04-10 16:43:03 +0300

    feat(feature): oneflow feature start finish

--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ export default {
 
   // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
   // https://rollupjs.org/guide/en#external-e-external
-  // external: [],
+  external: ['shelljs'],
 
   plugins,
 
