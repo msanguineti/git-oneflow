869a3d2785abfdf445f9e12be63f5c83a8ab6f51
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-04-10 16:43:03 +0300

    feat(feature): oneflow feature start finish

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,26 @@
-module.exports = function (api) {
-  api.cache(true);
+module.exports = function(api) {
+  api.cache(true)
 
-  const presets = [["@babel/env", {
-    "targets": {
-      "node": true
-    }
-  }],
-    "@babel/preset-typescript"]
-  const plugins = []
+  const presets = [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: true
+        }
+      }
+    ],
+    "@babel/typescript"
+  ]
+
+  const plugins = [
+    // "@babel/syntax-dynamic-import",
+    // ["dynamic-import-node", { noInterop: true }],
+    "@babel/proposal-class-properties"
+  ]
 
   return {
     presets,
     plugins
-  };
+  }
 }
--- src/gof.ts
+++ src/gof.ts
@@ -1,3 +1,11 @@
-export default (name: string): string => {
-  return `Hello ${name}`
+// Copyright (c) 2019 Mirco Sanguineti
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import shell from 'shelljs'
+
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git')
+  shell.exit(1)
 }
