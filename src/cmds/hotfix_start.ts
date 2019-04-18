770433e564f3cdbe19fe06df334447037817c917
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-04-18 20:43:56 +0300

    fix(hotfix): fix strings

--- src/cmds/hotfix_start.ts
+++ src/cmds/hotfix_start.ts
@@ -9,12 +9,13 @@ import { isValidBranchName } from '../core'
 import { exec } from 'shelljs'
 
 export default {
-  command: 'start <branchName> <from>',
-  desc: 'Start a new hotfix',
+  command: 'start <hotfixName> <from>',
+  desc:
+    'Start a new hotfix.\n<hotfixName> should be something like `2.3.1`.\n<from> should be a branch (e.g. develop) or a commit (e.g. 9af345)',
   builder: (yargs: any) => {},
   handler: (argv: { [key: string]: any }) => {
-    if (isValidBranchName(argv.branchName)) {
-      exec(`git checkout -b ${argv.hotfix}/${argv.branchName} ${argv.from}`)
+    if (isValidBranchName(argv.hotfixName)) {
+      exec(`git checkout -b ${argv.hotfix}/${argv.hotfixName} ${argv.from}`)
     }
   }
 }
