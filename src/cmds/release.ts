c168ee28daa19cc57832cf3ccb7d4d627bb0bd08
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-04-18 19:58:37 +0300

    feat(command): release command start and finish

---
+++ src/cmds/release.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2019 Mirco Sanguineti
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import start from './release_start'
+import finish from './release_finish'
+
+export default {
+  command: 'release <command>',
+  desc: 'Manage starting and finishing releases',
+  builder: function (yargs: any) {
+    return yargs.command(start).command(finish)
+  },
+  handler: function (argv: { [key: string]: any }) {}
+}
