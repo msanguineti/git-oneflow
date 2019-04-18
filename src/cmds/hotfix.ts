df83e2d8b5fd8d4ccdb4795e70c8323c29b3b96e
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-04-18 20:14:40 +0300

    feat(command): hotfix command start and finish

---
+++ src/cmds/hotfix.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2019 Mirco Sanguineti
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import start from './hotfix_start'
+import finish from './hotfix_finish'
+
+export default {
+  command: 'hotfix <command>',
+  desc: 'Manage starting and finishing hotfixes',
+  builder: function (yargs: any) {
+    return yargs.command(start).command(finish)
+  },
+  handler: function (argv: { [key: string]: any }) {}
+}
