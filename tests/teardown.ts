9917ea00b5caef4e21c4766b1803f0705dcf075a
msanguineti <19243840+msanguineti@users.noreply.github.com>
2020-06-24 14:45:54 +0300

    build: remove devdep npm-run-all

--- tests/teardown.ts
+++ tests/teardown.ts
@@ -1,6 +1,6 @@
 import * as shelljs from 'shelljs'
 
-export default async () => {
+export default async (): Promise<void> => {
   const glb = global as NodeJS.Global & { __STASHED__: boolean }
 
   if (glb.__STASHED__) shelljs.exec('git stash pop', { silent: true })
